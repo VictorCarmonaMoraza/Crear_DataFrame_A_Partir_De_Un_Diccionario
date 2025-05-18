07b0fa9c85af7ad4a13e7eaa4599f31477fbee4f
Victor Carmona <victorcarmona9035.com>
2025-05-18 20:10:55 +0200

    Exportar el daataframe en csv

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 
 info_pais ={"pais": ["Brasil", "Mexico", "Argentina", "Chile", "Colombia", "Peru"],
@@ -20,3 +21,10 @@ print(df.dtypes)
 
 # Ver las estadisticas del DataFrame
 print(df.describe())
+
+#uta exportacion Dataframe
+ruta_export = 'C:/Users/Victo/Desktop/DataCience/CreacionDataframeAPartirDeDiccionario/exportaciones'
+nombre_archivo = "info_pais.csv"
+ruta_completa = os.path.join(ruta_export, nombre_archivo)
+#Exportar el DataFrame a un archivo CSV
+df.to_csv(ruta_completa, sep=',', encoding='utf-8', index=True)
